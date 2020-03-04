95be6fed5d8726e93244d2c5d81485d89d8f7230
John Yi <sunny1983yz@gmail.com>
2020-03-04 16:15:02 +0800

    Update

--- api/src/app/Http/Controllers/Facebook/Messenger/MessageController.php
+++ api/src/app/Http/Controllers/Facebook/Messenger/MessageController.php
@@ -84,9 +84,7 @@ class MessageController extends MessengerController
 
                 $filePath = $file->storeAs('images/' . $pageId, $file->getClientOriginalName());
 
-                dd($filePath);
-
-                $response = (new FacebookMessage($this->accessToken))->sendFile($request->input('recipient'), $type, $filePath);
+                $response = (new FacebookMessage($this->accessToken))->sendFile($request->input('recipient'), $type, storage_path('app/' . $filePath));
             }
         }
 
